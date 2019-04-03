96472a6c81147880582907c11b8bade90302fa81
LuXiFa2017 <505184302@qq.com>
2019-04-03 16:34:21 +0800

    初次提交20190403

---
+++ app/src/main/java/com/mrhao/personalutiltest/myactivity/LoadingActivity.java
@@ -0,0 +1,56 @@
+package com.mrhao.personalutiltest.myactivity;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.mrhao.personalutiltest.MainActivity;
+import com.mrhao.personalutiltest.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @BindView(R.id.start_img)
+    ImageView startImg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+        ButterKnife.bind(this);
+        startImg.post(new Runnable() {
+            @Override
+            public void run() {
+                setBaseEvent();
+            }
+        });
+    }
+
+
+    private void setBaseEvent() {
+
+        //Android属性动画-渐变
+        ObjectAnimator alpha = ObjectAnimator.ofFloat(startImg, "alpha", 0f, 1f);
+        alpha.setDuration(2800);
+        alpha.start();
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoadingActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2800);
+
+    }
+
+}
